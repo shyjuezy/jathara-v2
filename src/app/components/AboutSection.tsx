8a1ef51eebd5659001cbc311b5e73963ef4e100a
shyju.viswambaran <shyju.viswambaran@coxautoinc.com>
2024-12-22 13:05:36 -0600

    added hidden back

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -1,22 +1,24 @@
-// import Image from "next/image";
+import Image from "next/image";
 
 export function AboutSection() {
   return (
     <section className="mt-16 py-16 bg-white">
       <div className="container mx-auto px-8">
         <div className="flex flex-col md:flex-row md:items-center gap-12">
-          <div className="hidden md:block relative w-full aspect-square md:aspect-[4/3] rounded-lg overflow-hidden shadow-lg">
-            {/* <Image
-              src="/about.jpg"
-              alt="Biryani on a pot"
-              className="object-cover w-full h-full"
-              width={800}
-              height={600}
-              quality={75}
-              sizes="(max-width: 768px) 100vw, 50vw"
-              priority
-            /> */}
-            <h3>About Section</h3>
+          <div className="hidden md:block">
+            <div className="relative w-full aspect-square md:aspect-[4/3] rounded-lg overflow-hidden shadow-lg">
+              <Image
+                src="/about.jpg"
+                alt="Biryani on a pot"
+                className="object-cover w-full h-full"
+                width={800}
+                height={600}
+                quality={75}
+                sizes="(max-width: 768px) 100vw, 50vw"
+                priority
+              />
+              <h3>About Section</h3>
+            </div>
           </div>
           <div className="w-full">
             <h2 className="text-3xl font-bold text-center md:text-left text-gray-800 mb-8 font-serif">
