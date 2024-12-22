7effe844285077284b2685c0f4e8421711088eb8
Shyju Viswambaran <8076655+shyjuezy@users.noreply.github.com>
2024-12-22 12:15:38 -0600

    Revert "Added About Section"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 import { Header } from '@/app/components/Header'
 import { Footer } from '@/app/components/Footer'
 import { HeroSection } from '@/app/components/HeroSection'
-import { AboutSection } from '@/app/components/AboutSection'
+
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow">
         <HeroSection />
-        <AboutSection />
       </main>
       <Footer />
     </div>
