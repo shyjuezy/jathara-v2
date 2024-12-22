b0e76b38f2e185f71f902edfd921c3849c385f1e
shyju.viswambaran <shyju.viswambaran@coxautoinc.com>
2024-12-22 12:53:48 -0600

    added hidden back

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -5,7 +5,7 @@ export function AboutSection() {
     <section className="mt-16 py-16 bg-white">
       <div className="container mx-auto px-8">
         <div className="flex flex-col md:flex-row md:items-center gap-12">
-          <div className="relative w-full aspect-square md:aspect-[4/3] rounded-lg overflow-hidden shadow-lg">
+          <div className="hidden md:block relative w-full aspect-square md:aspect-[4/3] rounded-lg overflow-hidden shadow-lg">
             <Image
               src="/about.jpg"
               alt="Biryani on a pot"
