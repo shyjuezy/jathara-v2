e9e4f1cbf09894e8265486eafd55d95b786f5bc7
shyju.viswambaran <shyju.viswambaran@coxautoinc.com>
2024-12-22 13:13:55 -0600

    added hidden back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { Header } from '@/app/components/Header'
 import { Footer } from '@/app/components/Footer'
 import { HeroSection } from '@/app/components/HeroSection'
 import { AboutSection } from '@/app/components/AboutSection'
+import { Gallery } from '@/app/components/gallery'
+
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
@@ -9,6 +11,7 @@ export default function Home() {
       <main className="flex-grow">
         <HeroSection />
         <AboutSection />
+        <Gallery />
       </main>
       <Footer />
     </div>
---
+++ src/data/gallery-images.ts
@@ -0,0 +1,53 @@
+export interface GalleryImage {
+    alt: string;
+    src: string;
+    rowSpan: string;
+}
+
+export const images: GalleryImage[] = [
+    {
+        alt: "Dosa",
+        src: "/images/gallery/dosa.jpg",
+        rowSpan: "row-span-3",
+    },
+    {
+        alt: "Idli",
+        src: "/images/gallery/idli.jpg",
+        rowSpan: "row-span-1",
+    },
+    {
+        alt: "Biriyani",
+        src: "/images/gallery/biriyani.jpg",
+        rowSpan: "row-span-4",
+    },
+    {
+        alt: "Butter Chicken",
+        src: "/images/gallery/butter_chicken.jpg",
+        rowSpan: "row-span-5",
+    },
+    {
+        alt: "Chicken Kebab",
+        src: "/images/gallery/chicken_kebab.jpg",
+        rowSpan: "row-span-5",
+    },
+    {
+        alt: "Egg Curry",
+        src: "/images/gallery/egg_curry.jpg",
+        rowSpan: "row-span-4",
+    },
+    {
+        alt: "Paneer Kebab",
+        src: "/images/gallery/paneer_kebab.jpg",
+        rowSpan: "row-span-3",
+    },
+    {
+        alt: "Snacks",
+        src: "/images/gallery/snacks.jpg",
+        rowSpan: "row-span-3",
+    },
+    {
+        alt: "Kebabs",
+        src: "/images/gallery/kebabs.jpg",
+        rowSpan: "row-span-2",
+    },
+];
